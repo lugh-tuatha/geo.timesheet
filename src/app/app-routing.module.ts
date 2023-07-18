f9fa0298facab85b6351e5879cc165ec35f0dc14
Ace Gabriel P. Pasiliao <114063817+lugh-tuatha@users.noreply.github.com>
2023-07-18 10:43:02 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TimesheetComponent } from './pages/timesheet/timesheet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TimesheetComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TimesheetComponent } from './pages/timesheet/timesheet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    DashboardComponent,
+    TimesheetComponent
   ],
   imports: [
     BrowserModule,
