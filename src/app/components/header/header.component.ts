43f0385ad9df094804c704323f113695fee649fa
Ace Gabriel P. Pasiliao <114063817+lugh-tuatha@users.noreply.github.com>
2023-07-18 15:24:55 +0800

    add: table hardcoded

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  faRightFromBracket = faRightFromBracket;
 }
