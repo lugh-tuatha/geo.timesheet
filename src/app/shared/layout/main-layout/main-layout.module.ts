a88125b94d87188c08878dae4934859acbf05113
Ace Gabriel P. Pasiliao <114063817+lugh-tuatha@users.noreply.github.com>
2023-08-11 10:53:17 +0800

    move header & sidebar components to layout

---
+++ src/app/shared/layout/main-layout/main-layout.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainLayoutComponent } from './main-layout.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    MainLayoutComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  exports: [
+    FooterComponent,
+    MainLayoutComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    AppRoutingModule,
+  ]
+})
+export class MainLayoutModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,32 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module';
-import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { ButtonModule } from '@ui';
+import { MainLayoutModule } from './layout/main-layout/main-layout.module';
 
 @NgModule({
-  declarations: [
-    SidebarComponent,
-    HeaderComponent,
-    FooterComponent,
-    MainLayoutComponent,
-    MainLayoutComponent,
-  ],
-  exports: [
-    SidebarComponent,
-    HeaderComponent,
-    FooterComponent,
-    MainLayoutComponent,
-    ButtonModule
-  ],
-  imports: [
-    CommonModule,
-    FontAwesomeModule,
-    AppRoutingModule
-  ]
+    declarations: [
+    ],
+    exports: [
+        ButtonModule,
+        MainLayoutModule
+    ],
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+        MainLayoutModule,
+    ]
 })
 export class SharedModule { }
